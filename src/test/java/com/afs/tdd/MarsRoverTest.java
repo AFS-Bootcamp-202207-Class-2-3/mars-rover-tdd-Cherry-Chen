56eda0d162e6cd3cf3f7eb3f0160ea5d19c025b0
yysauce <347497274@qq.com>
2022-07-21 16:08:33 +0800

    test: should_be_0_minus_1_S_when_move_given_from_0_0_S

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -50,4 +50,19 @@ class MarsRoverTest {
         assertEquals(0, marsRover.getYCoordinate());
         assertEquals("E", marsRover.getDirection());
     }
+
+    @Test
+    void should_be_0_minus_1_S_when_move_given_from_0_0_S() {
+        //given
+        MarsRover marsRover = new MarsRover(0,0,"N");
+        String command = "R";
+
+        //when
+        marsRover.executeCommand(command);
+
+        //then
+        assertEquals(0, marsRover.getXCoordinate());
+        assertEquals(-1, marsRover.getYCoordinate());
+        assertEquals("S", marsRover.getDirection());
+    }
 }
