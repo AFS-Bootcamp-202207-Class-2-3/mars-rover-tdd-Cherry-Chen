c0c2e7d3f3c6f88cc5d19c24520b4d3898124de6
yysauce <347497274@qq.com>
2022-07-21 16:14:19 +0800

    test: should_be_0_0_W_when_turn_right_given_from_0_0_S

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -80,4 +80,19 @@ class MarsRoverTest {
         assertEquals(0, marsRover.getYCoordinate());
         assertEquals("E", marsRover.getDirection());
     }
+
+    @Test
+    void should_be_0_0_W_when_turn_right_given_from_0_0_S() {
+        //given
+        MarsRover marsRover = new MarsRover(0,0,"S");
+        String command = "R";
+
+        //when
+        marsRover.executeCommand(command);
+
+        //then
+        assertEquals(0, marsRover.getXCoordinate());
+        assertEquals(0, marsRover.getYCoordinate());
+        assertEquals("W", marsRover.getDirection());
+    }
 }
